e816f14a2f73c874262fa6108ed7c3c445d7de12
Felipe Benghi <fbenghi@gmail.com>
2021-12-29 16:40:34 -0300

    Functions for creation of Testaments

--- contracts/Inheritance.sol
+++ contracts/Inheritance.sol
@@ -5,8 +5,56 @@ import "hardhat/console.sol";
 
 contract Inheritance {
 
-    constructor() {
-        console.log("Deploying a contract to handle Inheritance");
+    struct Testament {
+        address payable beneficiary;
+        uint deposit;
+        uint lastCounterReset;
+        uint maxCount;
     }
+
+    mapping(address => Testament) public testaments;
+
+    constructor() {
+        console.log("Contract deployed");
+    }
+
+    function setTestament(address payable _beneficiary, uint _maxCount) 
+        public
+        payable
+    {
+        Testament storage _testament = testaments[msg.sender];
+
+        // If sender already had deposits, add to previous
+        _testament.deposit     = msg.value+_testament.deposit;
+
+        // Other variables can be simply overwritten 
+        _testament.beneficiary = _beneficiary;        
+        _testament.lastCounterReset = block.timestamp;
+        _testament.maxCount    = _maxCount;
+        
+        
+        console.log("Asset Owner: ", msg.sender);
+        console.log("Beneficiary: ", testaments[msg.sender].deposit);
+        console.log("Last time counter was reset ", testaments[msg.sender].lastCounterReset);
+    }
+
+    function getTestament(address payable owner) 
+        public
+        view
+        returns (
+            address payable beneficiary,
+            uint deposit,
+            uint lastCounterReset,
+            uint maxCount
+        )
+        
+    {
+        return (testaments[owner].beneficiary,
+            testaments[owner].deposit,
+            testaments[owner].lastCounterReset,
+            testaments[owner].maxCount
+        );
+    }
+
 }
 
--- test/sample-test.js
+++ test/sample-test.js
@@ -8,3 +8,62 @@ describe("Deploy", function () {
     await inheritance.deployed();
   });
 });
+
+
+describe("SetTestament", function () {
+  it("Add new entry", async function () {
+    // Deploy
+    const Inheritance = await ethers.getContractFactory("Inheritance");
+    const inheritance = await Inheritance.deploy();
+    await inheritance.deployed();
+
+    // Accounts
+    const accounts = await ethers.getSigners();
+    const owner   = accounts[0];
+    const beneficiary = accounts[1];
+
+    // Send transaction
+    let overrides = {
+      // To convert Ether to Wei:
+      value: ethers.utils.parseEther("1.0")     // ether in this case MUST be a string
+    }
+    await inheritance.setTestament(beneficiary.address, 1000, overrides);
+
+    // Get values from contract
+    var test = await inheritance.getTestament(owner.address);
+
+    // Testing
+    expect(test[0]).to.equal(beneficiary.address);
+    expect(test[1]).to.equal(ethers.utils.parseEther("1.0") );
+  });
+});
+
+describe("SetTestament", function () {
+  it("Add value to existing entry", async function () {
+    // Deploy
+    const Inheritance = await ethers.getContractFactory("Inheritance");
+    const inheritance = await Inheritance.deploy();
+    await inheritance.deployed();
+
+    // Accounts
+    const accounts = await ethers.getSigners();
+    const owner   = accounts[0];
+    const beneficiary1 = accounts[1];
+    const beneficiary2 = accounts[1];
+
+    // Send transaction
+    let overrides = {
+      // To convert Ether to Wei:
+      value: ethers.utils.parseEther("1.0")     // ether in this case MUST be a string
+    }
+    await inheritance.setTestament(beneficiary1.address, 1000, overrides);
+    await inheritance.setTestament(beneficiary2.address, 1000, overrides);
+
+    // Get values from contract
+    var test = await inheritance.getTestament(owner.address);
+
+    // Testing
+    expect(test[0]).to.equal(beneficiary2.address);
+    expect(test[1]).to.equal(ethers.utils.parseEther("2.0") );
+  });
+});
