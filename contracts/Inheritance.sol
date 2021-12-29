cbbab80c25b6f65df0a3fa35a1f01a7d92651dc3
Felipe Benghi <fbenghi@gmail.com>
2021-12-29 15:00:35 -0300

    Added mocha and renamed renamed files from base project

---
+++ contracts/Inheritance.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Inheritance {
+
+    constructor() {
+        console.log("Deploying a contract to handle Inheritance");
+    }
+}
+
